0217b17da05852cb1982f3024dfbef4580398a57
mundosat <74665713+mundosat@users.noreply.github.com>
2026-03-27 19:47:22 -0500

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,8 +1,10 @@
+// firebase-config.js
+
 export const firebaseConfig = {
   apiKey: "AIzaSyB01VHK4m223ONUQHX8szla9msvSI34gSY",
   authDomain: "punto-venta-pro.firebaseapp.com",
   projectId: "punto-venta-pro",
-  storageBucket: "punto-venta-pro.firebasestorage.app",
+  storageBucket: "punto-venta-pro.appspot.com",
   messagingSenderId: "784099986059",
   appId: "1:784099986059:web:7a579dfb04ca2fc73fe28e"
 };
